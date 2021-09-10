a99581052b1c9a1787544bd6c5ed6d1a2f5b66f1
Rebeca Gaia <reb.gaia@gmail.com>
2021-09-10 16:27:37 -0300

    consumindo api usando ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,19 @@
-// SPA
-// SSR
-// SSG
-
 export default function Home(props) {
-  // * SPA
+  // * SPA - sigle page application
   /* useEffect(() => {
     fetch('http://localhost:3333/episodes')
     .then(res => res.json())
     .then(data => console.log(data))
   }, []); // hook responsável por disparar algo sempre que alguma coisa mudar na aplicação */
-
     
   return (
-    <h1></h1>
+    <h1>{JSON.stringify(props.episodes)}</h1>
+    
   )
 }
 
-export async function getServerSideProps() {
+// * SSR - server-side redering
+/* export async function getServerSideProps() {
   const response = await fetch('http://localhost:3333/episodes')
   const data = await response.json();
 
@@ -24,5 +21,18 @@ export async function getServerSideProps() {
     props: {
       episodes: data,
     }
+  }
+} */
+
+// * SSG - static site geneartion
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
   }
 }
