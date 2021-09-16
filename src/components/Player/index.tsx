da906c1ef2dfae27603da9de992218b9dd427d92
Rebeca Gaia <reb.gaia@gmail.com>
2021-09-15 22:16:28 -0300

    adicionando duration

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
@@ -6,9 +6,11 @@ import 'rc-slider/assets/index.css';
 import { usePlayer } from '../../contexts/PlayerContext';
 
 import styles from './styles.module.scss'
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
 export function Player() {
   const audioRef = useRef<HTMLAudioElement>(null);
+  const [progress, setProgress] = useState(0);
   
   const { 
     episodeList, 
@@ -39,7 +41,15 @@ export function Player() {
       audioRef.current.pause();
     }
 
-  }, [isPlaying])
+  }, [isPlaying]);
+
+  function setupProgressListener() {
+    audioRef.current.currentTime = 0;
+
+    audioRef.current.addEventListener('timeupdate', () => {
+      setProgress(Math.floor(audioRef.current.currentTime));
+    });
+  }
   
   return (
     <div className={styles.playerContainer}>
@@ -67,7 +77,7 @@ export function Player() {
 
       <footer className={!episode ? styles.empty : ''}>
         <div className={styles.progress}>
-          <span>00:00</span>
+          <span>{convertDurationToTimeString(progress)}</span>
           <div className={styles.slider}>
             { episode ? (
               <Slider
@@ -79,7 +89,7 @@ export function Player() {
               <div className={styles.emptySlider} />
             )}
           </div>
-          <span>00:00</span>
+          <span>{convertDurationToTimeString(episode?.duration ?? 0)}</span>
         </div>
         
         { episode && (
@@ -90,6 +100,7 @@ export function Player() {
             autoPlay
             onPlay={() => setIsPlayingState(true)}
             onPause={() => setIsPlayingState(false)}
+            onLoadedMetadata={setupProgressListener}
           />
         )}
         
